35cc6abd93ee9cca56bcca04781bec80265cddac
Fernando Leanos <60533313+fleanos@users.noreply.github.com>
2023-02-18 09:07:43 +0000

    update main to support multiprocessing

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from parseDocs import *
-from storeIndex import *
 from tokenizer import *
-from os import walk
-from os import path
+from os import walk, path
 import numpy as np
 import multiprocessing
+import pickle
 
 def findAllFiles(folder: str) -> set:
   return {path.join(root, file) for root, _, files in walk(folder, topdown=True) for file in files}
 
 def compute(files, threadNum):
+  tokenDict = dict()
   for i in files:
     for token, freq in tokenize(getStrings(i)):
-      storeData(i, token, freq, threadNum)
+      if token in tokenDict: tokenDict[token].add((i, freq))
+      else: tokenDict[token] = {(i, freq)}
+  pickle.dump(tokenDict, open("Index\Index" + threadNum + ".txt", "wb"), pickle.HIGHEST_PROTOCOL)
+  print(f"Process: {threadNum} - DONE")
+  
 
 def buildIndex(folderPath : str) -> None:
  
-  filePaths = np.array_split(list(findAllFiles(folderPath)), 6)
+  filePaths = np.array_split(list(findAllFiles(folderPath)), 8)
 
   p1 = multiprocessing.Process(target=compute, args=(filePaths[0], "1", ))
   p2 = multiprocessing.Process(target=compute, args=(filePaths[1], "2", ))
@@ -24,14 +28,14 @@ def buildIndex(folderPath : str) -> None:
   p4 = multiprocessing.Process(target=compute, args=(filePaths[3], "4", ))
   p5 = multiprocessing.Process(target=compute, args=(filePaths[4], "5", ))
   p6 = multiprocessing.Process(target=compute, args=(filePaths[5], "6", ))
-
+  
   p1.start()
   p2.start()
   p3.start()
   p4.start()
   p5.start()
   p6.start()
-  
+
   p1.join()
   p2.join()
   p3.join()
@@ -39,6 +43,7 @@ def buildIndex(folderPath : str) -> None:
   p5.join()
   p6.join()
 
+
 if __name__ == "__main__":
-  buildIndex("ANALYST")
+  buildIndex("developer")
   print("Done")
